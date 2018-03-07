67e878845980731b381035ac2cd875ae924a2a9c
Manu <serve.emmanuel@gmail.com>
2018-03-07 11:32:21 +0100

    Inscription dans la base de données

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+    // Connexion à la base de donnée
+     
+        try
+        {
+            $host = 'localhost';
+            $database = 'espacemembres';
+            $identifiant = 'root';
+            $password = 'root';
+            $db = new PDO('mysql:host='.$host.';dbname='.$database.';charset=utf8', $identifiant, $password);
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        }catch(PDOException $e){
+            echo 'La base de donnée n\'est pas disponible pour le moment. <br />';
+            echo ''.$e->getMessage().'<br />';
+            echo 'Ligne : '.$e->getLine();
+        }
+ 
+    // Fin de la connexion à la base de donnée
+?>
